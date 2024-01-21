d9404f5e7046162508ec6c1714a9f80e76c0326d
Anubhav Kundu <tuj00212@temple.edu>
2024-01-20 21:11:47 -0500

    added gpt verification functionality

--- gptverify.py
+++ gptverify.py
@@ -1,7 +1,10 @@
-import openai
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv() 
+client = OpenAI()
 
 base_prompt = """
-
 I need your assistance in determining whether or not the following job description is well suited for someone who is looking for ENTRY-LEVEL software engineering, data analyst, data engineering, data science jobs. 
 
 This person is going to be fresh graduate out of college (May 2024) with a Bachelor's in Computer Science and will have somewhere from 0 - 1 years of experience. This person is a U.S Citizen and does not require visa sponsorship. This person does not have an active security clearance.  Job descriptions requiring 2 years of experience can be considered 'YES' as well, but make sure to address all aspects of the job description. 
@@ -9,6 +12,17 @@ This person is going to be fresh graduate out of college (May 2024) with a Bache
 Assume the applicant has typical physical and technical aptitude. The applicant is knowledgeable with OOP principles, programming languages, software development tools and principles, web development, data analysis, database management, etc.
 
 You are to answer with ONLY 'YES' if the job description is well-suited for the applicant, or ONLY 'NO' otherwise.
-
-Job Description:
 """
+
+def evaluate(job_desc):
+    response = client.chat.completions.create(
+
+    model = 'gpt-3.5-turbo-1106',
+    messages = [
+        {'role': 'system', 'content': base_prompt},
+        {'role': 'user', 'content': job_desc}
+    ]
+)
+    return response.choices[0].message.content
+
+
